f51c5b292b6eb11b52f20f548a390dee6fcb48f8
Charlie Haslam <ch17873@my.bristol.ac.uk>
2019-10-22 15:22:30 +0100

    Basic Car Movement
    Created a simple car (with crap physcis) using a rigidbody and a basic platform to drive around on.

---
+++ ProjectFiles/Assets/Scripts/Car/DriveController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Comparers;
+
+public class DriveController : MonoBehaviour
+{
+    private Rigidbody rigid;
+    [SerializeField] private Transform steering;
+    [SerializeField] private float accel; //acceleration value for the car
+    [SerializeField] private float maxSpeed; //top speed
+
+    [SerializeField] private float turnFactor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // FixedUpdate is 50Hz
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Vertical"))
+        {
+            rigid.AddForce(steering.forward * accel * Input.GetAxis("Vertical"));
+        }
+
+        if (Input.GetButton("Horizontal"))
+        {
+            steering.Rotate(0f, turnFactor * Input.GetAxis("Horizontal") * Time.deltaTime, 0f);
+        }
+    }
+}
