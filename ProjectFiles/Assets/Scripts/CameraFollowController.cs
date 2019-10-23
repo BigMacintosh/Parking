bccb52f405bdb6112952d511dfb306d09527b620
Charlie Haslam <ch17873@my.bristol.ac.uk>
2019-10-23 13:24:59 +0100

    Better Driving
    Car feels much more useable, smooth follow camera. Friciton values need adjusting but feels less 'slippery' in general.#2

---
+++ ProjectFiles/Assets/Scripts/CameraFollowController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowController : MonoBehaviour {
+
+	
+	public void LookAtTarget()
+	{
+		Vector3 _lookDirection = objectToFollow.position - transform.position;
+		Quaternion _rot = Quaternion.LookRotation(_lookDirection, Vector3.up);
+		transform.rotation = Quaternion.Lerp(transform.rotation, _rot, lookSpeed * Time.deltaTime);
+	}
+
+	public void MoveToTarget()
+	{
+		Vector3 _targetPos = objectToFollow.position + 
+							 objectToFollow.forward * offset.z + 
+							 objectToFollow.right * offset.x + 
+							 objectToFollow.up * offset.y;
+		transform.position = Vector3.Lerp(transform.position, _targetPos, followSpeed * Time.deltaTime);
+	}
+
+	private void FixedUpdate()
+	{
+		LookAtTarget();
+		MoveToTarget();
+	}
+
+	public Transform objectToFollow;
+	public Vector3 offset;
+	public float followSpeed = 10;
+	public float lookSpeed = 10;
+}
