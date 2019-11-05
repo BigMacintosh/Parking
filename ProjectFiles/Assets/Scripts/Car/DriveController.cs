9a893222480ef0969055ce9f5110385953577a01
Dimmitsaras <35996393+Dimmitsaras@users.noreply.github.com>
2019-11-05 14:22:27 +0000

    Changed variable values to improve driving, added simple HUD, added a… (#15)
    * Changed variable values to improve driving, added simple HUD, added anti-rolling mechanics. And a ramp.
    
    * Removed serializedfield on public variable

--- ProjectFiles/Assets/Scripts/Car/DriveController.cs
+++ ProjectFiles/Assets/Scripts/Car/DriveController.cs
@@ -7,16 +7,27 @@ using UnityEngine.Assertions.Comparers;
 
 public class DriveController : MonoBehaviour
 {
+    public GameObject car;
+    private Rigidbody rb;
+    private float v;
+
     [SerializeField] private List<Axle> axles;
 
     [SerializeField] private float maxMotorTorque;
     [SerializeField] private float maxSteeringAngle;
-    
+
+    private void Start()
+    {
+        rb = car.GetComponent<Rigidbody>();
+        v = rb.velocity.magnitude * 3.6f; // km/h
+    }
+
     // FixedUpdate is 50Hz
     void FixedUpdate()
     {
+        
         float torque = Input.GetAxis("Vertical") * maxMotorTorque;
-        float steering = Input.GetAxis("Horizontal") * maxSteeringAngle;
+        float steering = (Input.GetAxis("Horizontal") * maxSteeringAngle) / TurnMultiplier(v);
 
         foreach (Axle axle in axles)
         {
@@ -33,6 +44,23 @@ public class DriveController : MonoBehaviour
             }
         }
     }
+
+    public float TurnMultiplier(float v)
+    {
+        if (v > 20)
+            return 4;
+        else if (v > 40)
+            return 5;
+        else if (v > 80)
+            return 7;
+        else if (v > 200)
+            return 10;
+        else if (v > 400)
+            return 20;
+        else
+            return 1;
+    }
+
 }
 
 [System.Serializable]
@@ -43,3 +71,4 @@ public class Axle
     public bool motor; //Does this wheel provide torque
     public bool steering; //Does this wheel provide steering
 }
+
