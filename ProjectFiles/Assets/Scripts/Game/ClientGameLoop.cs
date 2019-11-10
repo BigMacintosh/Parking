0808adb7501775175edfa4e1ff0954aef3cf5a75
Jack Bond-Preston <jackbondpreston@outlook.com>
2019-11-10 18:44:51 +0000

    Move client networking from ClientGameLoop to Client

--- ProjectFiles/Assets/Scripts/Game/ClientGameLoop.cs
+++ ProjectFiles/Assets/Scripts/Game/ClientGameLoop.cs
@@ -1,107 +1,49 @@
 ﻿using Network;
-using Unity.Collections;
-using Unity.Networking.Transport;
-using Unity.Networking.Transport.Utilities;
-using UnityEngine;
-using NetworkConnection = Unity.Networking.Transport.NetworkConnection;
-using UdpCNetworkDriver = Unity.Networking.Transport.GenericNetworkDriver<Unity.Networking.Transport.IPv4UDPSocket,Unity.Networking.Transport.DefaultPipelineStageCollection>;
 
 namespace Game
 {
     public class ClientGameLoop : IGameLoop
     {
-    
-        public UdpCNetworkDriver Driver;
-        public NetworkConnection Connection;
-        public bool Done;
-        private NetworkPipeline pipeline;
+        private Client client;
         private World world;
-        private Spawner spawner;
-        
+
         public bool Init(Spawner spawner, string[] args)
         {
-            Driver = new UdpCNetworkDriver(new ReliableUtility.Parameters { WindowSize = 32 });
-            pipeline = Driver.CreatePipeline(typeof(ReliableSequencedPipelineStage));
-            Connection = default(NetworkConnection);
-
-            // TODO: make server endpoint configurable
-#if UNITY_EDITOR
-            string ipAddress = "127.0.0.1";
-#else
-        string ipAddress = "18.191.231.10";
-#endif
-        
-            var endpoint = NetworkEndPoint.Parse(ipAddress, 25565);
-            Connection = Driver.Connect(endpoint);
-        
-            Debug.Log("Client: ClientGameLoop Init");
-            return true;
+            // Start server
+            client = new Client();
+            
+            #if UNITY_EDITOR
+                var success = client.Start();
+            #else
+                var success = client.Start("18.191.231.10");
+            #endif
+            
+            // Create world
+            world = new World(spawner);
+            
+            
+            return success;
         }
 
         public void Shutdown()
         {
-            Driver.Dispose();
+            client.Shutdown();
+//            world.Destroy();
         }
-        
+
         public void Update()
         {
-//            Debug.Log("ClientGameLoop: Update() call");
-            Driver.ScheduleUpdate().Complete();
-
-            if (!Connection.IsCreated)
-            {
-                if (!Done)
-                {
-                    Debug.Log($"Client: Something went wrong when connecting to .");
-                }
-                return;
-            }
-
-            DataStreamReader reader;
-            NetworkEvent.Type command;
-            while ((command = Connection.PopEvent(Driver, out reader)) != NetworkEvent.Type.Empty)
-            {
-                Debug.Log("Client: Command received");
-                if (command == NetworkEvent.Type.Connect)
-                {
-                    Debug.Log($"Client: Successfully connected to server.");
-                
-                    var value = 1;
-                    using (var writer = new DataStreamWriter(16, Allocator.Temp))
-                    {
-                        writer.Write((byte) ClientNetworkEvent.ClientHandshake);
-                        writer.Write(value);
-                        Driver.Send(pipeline, Connection, writer);
-                    }
-                }
-                else if (command == NetworkEvent.Type.Data)
-                {
-                    Debug.Log("Client: Data received");
-                    var readerContext = default(DataStreamReader.Context);
-                    reader.ReadByte(ref readerContext);
-                    var value = reader.ReadUInt(ref readerContext);
-                    Debug.Log($"Client: Got the value {value} back from the server.");
-                    Done = true;
-                    Connection.Disconnect(Driver);
-                    Connection = default(NetworkConnection);
-                }
-                else if (command == NetworkEvent.Type.Disconnect)
-                {
-                    Debug.Log($"Client: Disconnected from the server.");
-                    Connection = default(NetworkConnection);
-                }
-            }
+            client.HandleNetworkEvents();
         }
 
         public void FixedUpdate()
         {
-            
+        
         }
 
         public void LateUpdate()
         {
         
         }
-
     }
 }
---
+++ ProjectFiles/Assets/Scripts/Network/Client.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Networking.Transport;
+using Unity.Networking.Transport.Utilities;
+using UnityEngine;
+using NetworkConnection = Unity.Networking.Transport.NetworkConnection; 
+using UdpCNetworkDriver = Unity.Networking.Transport.GenericNetworkDriver<Unity.Networking.Transport.IPv4UDPSocket,Unity.Networking.Transport.DefaultPipelineStageCollection>;
+
+namespace Network
+{
+    public class Client
+    {
+        private UdpCNetworkDriver driver;
+        private NetworkConnection connection;
+        private NetworkPipeline pipeline;
+
+        private string serverIP;
+        private ushort serverPort;
+
+        private bool done;
+
+        public Client()
+        {
+            driver = new UdpCNetworkDriver(new ReliableUtility.Parameters { WindowSize = 32 });
+            pipeline = driver.CreatePipeline(typeof(ReliableSequencedPipelineStage));
+            done = false;
+        }
+
+        public bool Start(string ip = "127.0.0.1", ushort port = 25565)
+        {
+            serverIP = ip;
+            serverPort = port;
+            
+            Debug.Log($"Client: Connecting to {serverIP}:{serverPort}...");
+            
+            var endpoint = NetworkEndPoint.Parse(serverIP, serverPort);
+            connection = driver.Connect(endpoint);
+
+            return true;
+        }
+        
+        public void Shutdown()
+        {
+            Debug.Log("Client: Shutting down.");
+            done = true;
+            connection.Close(driver);
+            driver.Dispose();
+        }
+
+        public void HandleNetworkEvents()
+        {
+            driver.ScheduleUpdate().Complete();
+            
+            // Check connection is valid
+            if (!connection.IsCreated)
+            {
+                if (!done)
+                {
+                    Debug.Log($"Client: Something went wrong when connecting to {serverIP}:{serverPort}.");
+                }
+                
+                return;
+            }
+
+            NetworkEvent.Type command;
+            while ((command = connection.PopEvent(driver, out var reader)) != NetworkEvent.Type.Empty)
+            {
+                Debug.Log("Client: Command received.");
+
+                switch (command)
+                {
+                    case NetworkEvent.Type.Connect:
+                    {
+                        Debug.Log($"Client: Successfully connected to {serverIP}:{serverPort}.");
+                
+                        var value = 1;
+                        using (var writer = new DataStreamWriter(16, Allocator.Temp))
+                        {
+                            writer.Write((byte) ClientNetworkEvent.ClientHandshake);
+                            writer.Write(value);
+                            driver.Send(pipeline, connection, writer);
+                        }
+
+                        break;
+                    }
+                    case NetworkEvent.Type.Data:
+                    {
+                        var readerContext = default(DataStreamReader.Context);
+                        var ev = (ServerNetworkEvent) reader.ReadByte(ref readerContext);
+                        HandleEvent(ev, reader, readerContext);
+
+                        break;
+                    }
+                    case NetworkEvent.Type.Disconnect:
+                        Debug.Log($"Client: Disconnected from the server.");
+                        connection = default(NetworkConnection);
+                        break;
+                }
+            }
+        }
+        
+        private void HandleEvent(ServerNetworkEvent ev, DataStreamReader reader, DataStreamReader.Context readerContext)
+        {
+            switch (ev)
+            {
+                case ServerNetworkEvent.ServerHandshake:
+                    var number = reader.ReadUInt(ref readerContext);
+                    Debug.Log($"Client: Received {number} back from {serverIP}:{serverPort}.");
+
+                    done = true;
+                    connection = default(NetworkConnection);
+                    
+                    break;
+                case ServerNetworkEvent.ServerLocationUpdate:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(ev), ev, null);
+            }
+        }
+    }
+}
