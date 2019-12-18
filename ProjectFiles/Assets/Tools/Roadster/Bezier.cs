34b4cbefdde583bc44f05ccf75c59471dd91e8b6
Charlie Haslam <ch17873@my.bristol.ac.uk>
2019-12-18 15:14:37 +0000

    Adding the files from the Roadster branch to do a pull request

---
+++ ProjectFiles/Assets/Tools/Roadster/Bezier.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Bezier
+{
+    //Get a point t distance along the curve
+    public static Vector3 GetPoint (Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) {
+        t = Mathf.Clamp01(t);
+        float oneMinusT = 1f - t;
+        return
+            oneMinusT * oneMinusT * oneMinusT * p0 +
+            3f * oneMinusT * oneMinusT * t * p1 +
+            3f * oneMinusT * t * t * p2 +
+            t * t * t * p3;
+    }
+    
+    //Calculate the First Derivative to get the direction of curves
+    public static Vector3 GetFirstDerivative (Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) {
+        t = Mathf.Clamp01(t);
+        float oneMinusT = 1f - t;
+        return
+            3f * oneMinusT * oneMinusT * (p1 - p0) +
+            6f * oneMinusT * t * (p2 - p1) +
+            3f * t * t * (p3 - p2);
+    }
+}
+
