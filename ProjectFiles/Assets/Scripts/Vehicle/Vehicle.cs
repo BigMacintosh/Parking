8bc3d5ee3dd1b96805fe3944bb6b4ff78509966d
Charlie Haslam <ch17873@my.bristol.ac.uk>
2019-12-22 18:41:34 +0800

    Working forwards and backwards, except the car seems to take off on bumpy roads... #59

--- ProjectFiles/Assets/Scripts/Vehicle/Vehicle.cs
+++ ProjectFiles/Assets/Scripts/Vehicle/Vehicle.cs
@@ -6,18 +6,18 @@ namespace Vehicle
 {
     public class Vehicle : MonoBehaviour
     {
-        public List<Axle> axles;
         public VehicleProperties vehicleProperties;
-        private DriveController driveController;
+        private VehicleDriver driver;
 
 
         public void SetControllable()
         {
             // Add DriveController to car
-            //driveController = gameObject.AddComponent<DriveController>();
-            gameObject.AddComponent<VehicleDriver>();
-            //driveController.Axles = axles;
-            
+            driver  = gameObject.AddComponent<VehicleDriver>();
+
+            driver.accel = 5;
+            driver.maxSpeed = 30;
+
             // Set camera to follow car
             var camera = FindObjectOfType<CameraFollowController>();
             camera.ObjectToFollow = transform;
@@ -28,17 +28,6 @@ namespace Vehicle
         }
     }
     
-    // Stores axles
-    [Serializable]
-    public class Axle
-    {
-        [field: SerializeField] public WheelCollider LeftWheel { get; private set; }
-        [field: SerializeField] public WheelCollider RightWheel { get; private set; }
-        [field: SerializeField] public bool Motor { get; private set; } // Does this wheel provide torque
-        [field: SerializeField] public bool Steering { get; private set; } // Does this wheel provide steering
-    }
-
-
     // Stores properties for each type of car
     // Allows drive controller to change based on different properties.
     [Serializable]
--- ProjectFiles/Assets/Scripts/Vehicle/VehicleDriver.cs
+++ ProjectFiles/Assets/Scripts/Vehicle/VehicleDriver.cs
@@ -2,23 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VehicleDriver : MonoBehaviour
+namespace Vehicle
 {
-    private Rigidbody body;
-    
-    //Car Properties
+    public class VehicleDriver : MonoBehaviour
+    {
+        private Rigidbody body;
 
-    [SerializeField] private float maxSpeed, accel, decel, maxSteer, steer;
+        //Car Properties
+
+        public float maxSpeed, accel, maxSteer, steer;
+
+        private float curSpeed;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        body = gameObject.GetComponent<Rigidbody>();
-    }
+        void Start()
+        {
+            body = gameObject.GetComponent<Rigidbody>();
+        }
 
-    void FixedUpdate()
-    {
-        var drive = transform.forward * 10f;
-        drive.y = body.velocity.y;
-        body.velocity = drive;
+        void FixedUpdate()
+        {
+            Debug.Log("Assigning Speed: " + curSpeed);
+            if (Input.GetAxis("Vertical") != 0)
+            {
+                curSpeed = Input.GetAxis("Vertical") > 0 ? Mathf.Min(curSpeed + Input.GetAxis("Vertical") * accel * Time.deltaTime, maxSpeed) : Mathf.Max(curSpeed + Input.GetAxis("Vertical") * accel * Time.deltaTime, -maxSpeed);
+
+                Vector3 vel = new Vector3(0f, body.velocity.y, 0f) + transform.forward * curSpeed;
+
+                body.velocity = vel;
+            }
+            else
+            {
+                
+                curSpeed += curSpeed > 0 ? - accel * Time.deltaTime : accel * Time.deltaTime;
+                
+
+                Vector3 vel = new Vector3(0f, body.velocity.y, 0f) + transform.forward * curSpeed;
+
+                body.velocity = vel;
+            }
+        }
     }
 }
