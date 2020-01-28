4fdae78f453f50efce7ea655571b1947dc9ce378
Charlie Haslam <ch17873@my.bristol.ac.uk>
2020-01-28 18:31:05 +0000

    Saving the raycast for handling between hits

--- ProjectFiles/Assets/Scripts/Vehicle/DriveWheel.cs
+++ ProjectFiles/Assets/Scripts/Vehicle/DriveWheel.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using UnityEngine;
 
 namespace Vehicle
@@ -8,16 +10,24 @@ namespace Vehicle
     {
 
         private float groundDist;
+        private RaycastHit hit;
+
+        private void OnControllerColliderHit(ControllerColliderHit hit)
+        {
+            throw new NotImplementedException();
+        }
+
         // Start is called before the first frame update
         void Start()
         {
-            groundDist = gameObject.GetComponent<Collider>().bounds.max.y - gameObject.GetComponent<Collider>().bounds.center.y + 0.1f;
+            groundDist = gameObject.GetComponent<Collider>().bounds.max.y -
+                         gameObject.GetComponent<Collider>().bounds.center.y + 0.1f;
         }
 
         public bool CheckGround()
         {
             Debug.DrawRay(transform.position, -Vector3.up * groundDist, Color.blue, 1f, false);
-            return Physics.Raycast(transform.position, -Vector3.up, groundDist);
+            return Physics.Raycast(new Ray(transform.position, -Vector3.up), out hit, groundDist);
 
         }
     }
