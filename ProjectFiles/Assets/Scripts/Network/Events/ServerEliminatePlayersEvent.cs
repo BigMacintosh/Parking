9d3b387c326f1ce921cef5bb884b1833dd6bc418
Liam Dalgarno <32525406+liamdalg@users.noreply.github.com>
2020-01-31 16:44:27 +0000

    Extra packets (#74)
    Co-authored-by: Jack Bond-Preston <jackbondpreston@outlook.com>
    Co-authored-by: Riley Evans <riley@rileyevans.co.uk>

---
+++ ProjectFiles/Assets/Scripts/Network/Events/ServerEliminatePlayersEvent.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Unity.Networking.Transport;
+
+namespace Network.Events
+{
+    public class ServerEliminatePlayersEvent : Event
+    {
+        public ushort RoundNumber { get; private set; }
+        public List<ushort> Players { get; private set; }
+
+        public ServerEliminatePlayersEvent()
+        {
+            ID = EventType.ServerEliminatePlayersEvent;
+            Length = 1;
+        }
+
+        public ServerEliminatePlayersEvent(ushort roundNumber, List<ushort> players)
+        {
+            RoundNumber = roundNumber;
+            Players = players;
+            Length = sizeof(ushort) * (Players.Count + 2);
+        }
+
+        public override void Serialise(DataStreamWriter writer)
+        {
+            base.Serialise(writer);
+            writer.Write(RoundNumber);
+            writer.Write((ushort) Players.Count);
+            foreach (var p in Players)
+            {
+                writer.Write(p);
+            }
+        }
+
+        public override void Deserialise(DataStreamReader reader, ref DataStreamReader.Context context)
+        {
+            RoundNumber = reader.ReadByte(ref context);
+
+            var playerCount = reader.ReadByte(ref context);
+            Players = new List<ushort>();
+            for (int i = 0; i < playerCount; i++)
+            {
+                Players.Add(reader.ReadByte(ref context));
+            }
+            Length = sizeof(ushort) * (Players.Count + 2);
+        }
+
+        public override void Handle(Server server, NetworkConnection connection)
+        {
+            server.Handle(this, connection);
+        }
+
+        public override void Handle(Client client, NetworkConnection connection)
+        {
+            client.Handle(this, connection);
+        }
+    }
+}
---
+++ ProjectFiles/Assets/Scripts/Network/Events/ServerPreRoundStartEvent.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using Unity.Networking.Transport;
+
+namespace Network.Events
+{
+    public class ServerPreRoundStartEvent : Event
+    {
+        public ushort RoundNumber { get; private set; }
+        public ushort PreRoundLength { get; private set; }
+        public ushort RoundLength { get; private set; }
+        public ushort PlayerCount { get; private set; }
+        public List<ushort> Spaces { get; private set; }
+
+        public ServerPreRoundStartEvent()
+        {
+            ID = EventType.ServerPreRoundStartEvent;
+            Length = 1;
+        }
+
+        public ServerPreRoundStartEvent(ushort roundNumber, ushort preRoundLength, ushort roundLength, ushort playerCount, List<ushort> spaces) : this()
+        {
+            RoundNumber = roundNumber;
+            PreRoundLength = preRoundLength;
+            RoundLength = roundLength;
+            PlayerCount = playerCount;
+            Spaces = spaces;
+            Length = sizeof(ushort) * (Spaces.Count + 5);
+        }
+
+        public override void Serialise(DataStreamWriter writer)
+        {
+            base.Serialise(writer);
+            writer.Write(RoundNumber);
+            writer.Write(PreRoundLength);
+            writer.Write(RoundLength);
+            writer.Write(PlayerCount);
+            writer.Write((byte) Spaces.Count);
+            foreach (var s in Spaces)
+            {
+                writer.Write(s);
+            }
+        }
+
+        public override void Deserialise(DataStreamReader reader, ref DataStreamReader.Context context)
+        {
+            RoundNumber = reader.ReadUShort(ref context);
+            PreRoundLength = reader.ReadUShort(ref context);
+            RoundLength = reader.ReadUShort(ref context);
+            PlayerCount = reader.ReadUShort(ref context);
+
+            var spacesCount = reader.ReadUShort(ref context);
+            Spaces = new List<ushort>();
+            for (int i = 0; i < spacesCount; i++)
+            {
+                Spaces.Add(reader.ReadUShort(ref context));
+            }
+            Length = sizeof(ushort) * (Spaces.Count + 5);
+        }
+
+        public override void Handle(Server server, NetworkConnection connection)
+        {
+            server.Handle(this, connection);
+        }
+
+        public override void Handle(Client client, NetworkConnection connection)
+        {
+            client.Handle(this, connection);
+        }
+    }
+}
