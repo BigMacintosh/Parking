0bb9d2727a1f0473c12921412b19ed433910d02c
Dimmitsaras <dp17535@my.bristol.ac.uk>
2020-02-06 19:33:41 +0000

    monobehavior menu on latest build

---
+++ ProjectFiles/Assets/Scripts/LoadSceneButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadSceneButton : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        //sceneName = "TestScene";
+        SceneManager.LoadScene(sceneName);
+    }
+}
