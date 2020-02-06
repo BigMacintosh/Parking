6813abb92d83e3797b6ee300274a108c469b72bd
Dimmitsaras <dp17535@my.bristol.ac.uk>
2020-02-06 19:33:41 +0000

    monobehavior menu on latest build

---
+++ ProjectFiles/Assets/Scripts/escapemenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class escapemenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject escmenu;
+    void Start()
+    {
+        escmenu = Resources.Load<GameObject>("EscapeMenu");
+        escmenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+            if (Cursor.visible)
+            {
+                escmenu.SetActive(false);
+                Cursor.visible = false;
+            }
+            else
+            {
+                Cursor.visible = true;
+                escmenu.SetActive(true);
+            }
+    }
+}
