f65b8132886128ec62b876c98b1a4c033199da17
Riley Evans <riley@rileyevans.co.uk>
2020-02-14 17:55:50 +0000

    Adds build folder

---
+++ ProjectFiles/Assets/Scripts/Build/ServerBuild.cs
@@ -0,0 +1,49 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+
+namespace Build
+{
+    public class ServerBuild : MonoBehaviour
+    {
+        [MenuItem("Build/Build Server")]
+        public static void Build()
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = new[] { "Assets/Scene/ModelShowcase.unity" };
+
+            var buildFolder = GetArg("-buildFolder");
+
+            buildPlayerOptions.locationPathName = buildFolder;
+
+            buildPlayerOptions.target = BuildTarget.StandaloneLinux64;
+            buildPlayerOptions.options = BuildOptions.None;
+
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+            }
+
+            if (summary.result == BuildResult.Failed)
+            {
+                Debug.Log("Build failed");
+            }
+        }
+
+        private static string GetArg(string name)
+        {
+            var args = System.Environment.GetCommandLineArgs();
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == name && args.Length > i + 1)
+                {
+                    return args[i + 1];
+                }
+            }
+            return null;
+        }
+    }
+}
