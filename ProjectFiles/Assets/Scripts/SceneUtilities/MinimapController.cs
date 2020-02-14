135d6433f8980ba467af629325ccadfb32f42ef5
Liam Dalgarno <liamdalg99@gmail.com>
2020-02-14 17:43:13 +0000

    Minimap camera rotation is now constant
    Co-authored-by: Charlie Haslam <charliehasalamb@icloud.com>

---
+++ ProjectFiles/Assets/Scripts/SceneUtilities/MinimapController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace SceneUtilities
+{
+    public class MinimapController : MonoBehaviour
+    {
+        [SerializeField] private Vector3 offset;
+
+        public Transform ObjectToFollow { get; set; }
+
+        public void Start()
+        {
+            ObjectToFollow = FindObjectOfType<Game.Game>().transform;
+        }
+
+        public void FixedUpdate()
+        {
+            transform.position = ObjectToFollow.position + offset;
+        }
+    }
+}
