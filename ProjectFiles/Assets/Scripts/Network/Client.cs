90e08627cdafcf477e32c96657497d5024b0ef1c
Riley Evans <riley@rileyevans.co.uk>
2020-02-17 22:44:25 +0000

    Moves the location of the network delegates (#134)

--- ProjectFiles/Assets/Scripts/Network/Client.cs
+++ ProjectFiles/Assets/Scripts/Network/Client.cs
@@ -15,16 +15,7 @@ using UdpCNetworkDriver = Unity.Networking.Transport.GenericNetworkDriver<Unity.
 namespace Network
 {
     
-    public delegate void GameStartDelegate(ushort nPlayers);
     
-    public delegate void PreRoundStartDelegate(
-        ushort roundNumber, ushort preRoundLength, ushort roundLength, ushort nPlayers, List<ushort> spacesActive);
-    
-    public delegate void RoundStartDelegate(ushort roundNumber);
-    
-    public delegate void RoundEndDelegate(ushort roundNumber);
-    
-    public delegate void EliminatePlayersDelegate(ushort roundNumber, List<ushort> eliminatedPlayers);
 
 
     public interface IClient
