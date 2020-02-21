c0200e382a671ef2afb60b8b2740a4ec3ae59150
Chris Gora <kg17815@my.bristol.ac.uk>
2020-02-21 18:38:00 +0000

    Added ServerSpaceClaimedEvent

--- ProjectFiles/Assets/Scripts/Network/EventType.cs
+++ ProjectFiles/Assets/Scripts/Network/EventType.cs
@@ -6,10 +6,12 @@ namespace Network
     public enum EventType
     {
         Undefined = 0xFF,
+
         ClientHandshake = 0x81,
         ClientLocationUpdate = 0x82,
         ClientSpaceEnterEvent = 0x83,
         ClientSpaceExitEvent = 0x84,
+
         ServerHandshake = 0x01,
         ServerLocationUpdate = 0x02,
         ServerSpawnPlayerEvent = 0x03,
@@ -21,5 +23,7 @@ namespace Network
         ServerGameEndEvent          = 0x09,
         ServerKeepAliveEvent        = 0x0A,
         ServerGameStartEvent        = 0x0B,
+        ServerDisconnectEvent = 0x08,
+        ServerSpaceClaimedEvent = 0x09,
     }
 }
