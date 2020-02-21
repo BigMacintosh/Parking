bd6318595df68efd64af95072d16f0e9b0a3868b
Chris Gora <kg17815@my.bristol.ac.uk>
2020-02-21 19:12:54 +0000

    Bugfixes

--- ProjectFiles/Assets/Scripts/Network/EventType.cs
+++ ProjectFiles/Assets/Scripts/Network/EventType.cs
@@ -6,12 +6,10 @@ namespace Network
     public enum EventType
     {
         Undefined = 0xFF,
-
         ClientHandshake = 0x81,
         ClientLocationUpdate = 0x82,
         ClientSpaceEnterEvent = 0x83,
         ClientSpaceExitEvent = 0x84,
-
         ServerHandshake = 0x01,
         ServerLocationUpdate = 0x02,
         ServerSpawnPlayerEvent = 0x03,
@@ -19,11 +17,10 @@ namespace Network
         ServerRoundStartEvent = 0x05,
         ServerRoundEndEvent = 0x06,
         ServerEliminatePlayersEvent = 0x07,
-        ServerDisconnectEvent       = 0x08,
-        ServerGameEndEvent          = 0x09,
-        ServerKeepAliveEvent        = 0x0A,
-        ServerGameStartEvent        = 0x0B,
         ServerDisconnectEvent = 0x08,
-        ServerSpaceClaimedEvent = 0x09,
+        ServerGameEndEvent = 0x09,
+        ServerGameStartEvent = 0x0B,
+        ServerKeepAliveEvent = 0x0A,
+        ServerSpaceClaimedEvent = 0x0C,
     }
 }
