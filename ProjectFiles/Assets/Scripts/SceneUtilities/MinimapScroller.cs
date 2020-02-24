ab51b59e51615feee29fd6703c90054cdfe95074
Liam Dalgarno <liamdalg99@gmail.com>
2020-02-24 19:28:41 +0000

    Working on MinimapScroller and RenderToMinimap

---
+++ ProjectFiles/Assets/Scripts/SceneUtilities/MinimapScroller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SceneUtilities
+{
+    public class MinimapScroller : MonoBehaviour
+    {
+        [field: Header("World Anchor Points")] 
+        [field: SerializeField] public Vector2 topLeft { get; private set; } = new Vector2(250, -250);
+        [field: SerializeField] public Vector2 bottomRight { get; private set;  } = new Vector2(-250, 250);
+
+        public Vector2 anchorDifference { get; private set; }
+        
+        public void Start()
+        {
+            anchorDifference = topLeft - bottomRight;
+            // top left of image is (width / 2), (-height / 2)
+            // bot right of image is (-width / 2), (height / 2)
+        }
+
+        public void Update()
+        {
+            
+        }
+    }
+
+}
+
---
+++ ProjectFiles/Assets/Scripts/SceneUtilities/RenderToMinimap.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SceneUtilities
+{
+    public class RenderToMinimap : MonoBehaviour
+    {
+        [SerializeField] private Sprite sprite;
+        [SerializeField] private Color colour;
+
+        private RectTransform mapRect;
+        private GameObject indicator;
+        
+        public void Start()
+        {
+            // find the mask and add the sprite as a child
+            var mask = GameObject.Find("Minimap Mask");
+            indicator = new GameObject($"{name} Indicator");
+            indicator.AddComponent<Image>();
+            var image = indicator.GetComponent<Image>();
+            image.sprite = sprite;
+            indicator.transform.SetParent(mask.transform);
+
+            mapRect = mask.transform.GetChild(0).GetComponent<RectTransform>();
+        }
+
+        public void Update()
+        {
+        }
+    }
+}
+
