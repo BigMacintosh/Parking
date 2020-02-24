026cc2408841659628243dcbbeecb47e5ad2b1b9
liamdalg <liamdalg99@gmail.com>
2020-02-24 23:40:12 +0000

    simplify scroll logic

--- ProjectFiles/Assets/Scripts/SceneUtilities/MinimapScroller.cs
+++ ProjectFiles/Assets/Scripts/SceneUtilities/MinimapScroller.cs
@@ -8,27 +8,24 @@ namespace SceneUtilities
         [field: SerializeField] public Vector2 TopLeft { get; private set; } = new Vector2(-250, 250);
         [field: SerializeField] public Vector2 BottomRight { get; private set; } = new Vector2(250, -250);
 
-        public Vector2 AnchorDifference { get; private set; }
+        public float XScale { get; private set; }
+        public float YScale { get; private set; }
         public Transform ObjectToFollow { get; set; }
 
-        private RectTransform rectTransform;
+        // private RectTransform rectTransform;
 
         public void Awake()
         {
-            AnchorDifference = TopLeft - BottomRight;
+            var rect = ((RectTransform) transform).rect;
+            XScale = (BottomRight.x - TopLeft.x) / rect.width;
+            YScale = (TopLeft.y - BottomRight.y) / rect.height;
+            Debug.Log(XScale);
             ObjectToFollow = FindObjectOfType<Game.Game>().transform;
-            rectTransform = ((RectTransform) transform);
         }
 
         public void Update()
         {
-            // calculate the proportion of the map that you've travelled across from the top left
-            // this probs could be simplified but DON'T TOUCH
-            // NOTE: 2D Y coord = 3D Z coord
-            var xRatio = 1 - (TopLeft.x - ObjectToFollow.position.x) / AnchorDifference.x;
-            var yRatio = 1 - (TopLeft.y - ObjectToFollow.position.z) / AnchorDifference.y;
-            var rect = rectTransform.rect;
-            transform.localPosition = new Vector2(rect.width / 2 - xRatio * rect.width, -rect.height / 2 + yRatio * rect.height);
+            transform.localPosition = new Vector2(ObjectToFollow.position.x / XScale, ObjectToFollow.position.z / YScale);
         }
     }
 }
