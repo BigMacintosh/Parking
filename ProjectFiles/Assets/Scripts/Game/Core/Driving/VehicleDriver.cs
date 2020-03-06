a965bc9560f4c286c17c8585ebf4c6b052ff2f58
Charlie Haslam <ch17873@my.bristol.ac.uk>
2020-03-06 14:01:54 +0000

    Removed threshold

--- ProjectFiles/Assets/Scripts/Game/Core/Driving/VehicleDriver.cs
+++ ProjectFiles/Assets/Scripts/Game/Core/Driving/VehicleDriver.cs
@@ -18,7 +18,6 @@ namespace Game.Core.Driving {
         private float     turn;
         private float     maxTurn;
         private float     collisionAmplifier = 50f;
-        private float     collisionThresh;
         private float     collisionCooldown  = 0.5f;
         private float     timestamp          = 0f;
 
@@ -26,7 +25,6 @@ namespace Game.Core.Driving {
         private void Start() {
             body              = gameObject.GetComponent<Rigidbody>();
             body.centerOfMass = transform.Find("centreOfMass").transform.localPosition;
-            collisionThresh = body.mass / 2;
         }
 
         private void FixedUpdate() {
@@ -94,14 +92,11 @@ namespace Game.Core.Driving {
 
                 float myMomentum    = Mathf.Abs(Vector3.Dot(body.velocity,      colDir)) * body.mass;
                 float otherMomentum = Mathf.Abs(Vector3.Dot(otherBody.velocity, colDir)) * otherBody.mass;
-
-                if (Mathf.Abs(myMomentum - otherMomentum) > collisionThresh) {
-                    if (myMomentum < otherMomentum) {
-                        body.AddForce(colDir * otherMomentum * collisionAmplifier);
-                    }
-
-                    timestamp = Time.time + collisionCooldown;
+                
+                if (myMomentum < otherMomentum) {
+                    body.AddForce(colDir * otherMomentum * collisionAmplifier);
                 }
+                timestamp = Time.time + collisionCooldown;
             }
         }
     }
