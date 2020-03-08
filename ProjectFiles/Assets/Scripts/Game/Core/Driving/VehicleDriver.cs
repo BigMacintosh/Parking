54e9ba7d874c1cb6785ad9d9d97c56cc93f3fa23
Charlie Haslam <ch17873@my.bristol.ac.uk>
2020-03-08 19:41:29 +0000

    Tweaked centre of Gravity and wheel collider values, changed VehicleDriver to new system

--- ProjectFiles/Assets/Scripts/Game/Core/Driving/VehicleDriver.cs
+++ ProjectFiles/Assets/Scripts/Game/Core/Driving/VehicleDriver.cs
@@ -1,20 +1,25 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 namespace Game.Core.Driving {
     public class VehicleDriver : MonoBehaviour {
         // Public Fields
-        public float                                       maxSpeed;
-        public float                                       accel;
-        public float                                       maxSteer;
-        public float                                       driftFactor;
-        public System.Collections.Generic.List<DriveWheel> driveWheels;
-        public LayerMask                                   mask;
+        public List<WheelCollider> frontWheels;
+        public List<WheelCollider> rearWheesl;
+        public List<Transform>     frontWheelGraphics;
+        public List<Transform>     rearWheelGraphics;
+        public LayerMask                                      mask;
+        
+        public float                                          maxSteerAngle;
+        public float                                          motorForce;
 
         // Private Fields
         private bool      acceptinput;
         private Rigidbody body;
-        private float     turn;
-        private float     maxTurn;
+        
+        private float     horizontalInput;
+        private float     verticalInput;
+        
         private float     collisionAmplifier = 50f;
         private float     collisionCooldown  = 0.5f;
         private float     timestamp          = 0f;
@@ -26,39 +31,6 @@ namespace Game.Core.Driving {
         }
 
         private void FixedUpdate() {
-            var grounded = false;
-            turn = maxSteer * Input.GetAxis("Horizontal");
-
-            foreach (var wheel in driveWheels) {
-                grounded                                 = wheel.CheckGround() ? true : grounded;
-                wheel.gameObject.transform.localRotation = Quaternion.Euler(0, turn, 0);
-            }
-
-            //Driving Forces
-            if (acceptinput && Input.GetAxis("Vertical") != 0 && grounded && Mathf.Abs(GetForward()) < maxSpeed) {
-                body.AddForce(body.mass * accel * Input.GetAxis("Vertical") * transform.forward);
-            }
-
-            //Turning Forces
-            if (Input.GetAxis("Horizontal") != 0 && grounded && body.angularVelocity.magnitude < maxSteer / 30f &&
-                Mathf.Abs(GetForward())     > 0.5f) {
-                body.AddTorque(turn * body.mass * 2f * (GetForward() / Mathf.Abs(GetForward())) * transform.up);
-            }
-
-            if (acceptinput && Input.GetAxis("Jump") != 0 && grounded) {
-                body.AddForce(body.mass * accel * Input.GetAxis("Jump") * transform.up * 10f);
-            }
-
-            //Drifting
-            if (grounded) {
-                if (Input.GetAxis("Drift") != 0) {
-                    maxTurn = maxSteer * driftFactor / 30f;
-                } else {
-                    maxTurn       =  maxSteer          / 30f;
-                    body.velocity -= GetSide() * 0.25f * transform.right;
-                }
-            }
-
             //Self-Righting
             if (Mathf.Abs(body.rotation.eulerAngles.z) > 45 && Mathf.Abs(body.rotation.eulerAngles.z) < 315 ||
                 Mathf.Abs(body.rotation.eulerAngles.x) > 60 && Mathf.Abs(body.rotation.eulerAngles.x) < 300) {
@@ -69,14 +41,6 @@ namespace Game.Core.Driving {
                 body.MoveRotation(Quaternion.RotateTowards(
                                       body.rotation, targetRotation, Time.fixedDeltaTime * 90f));
             }
-        }
-
-        private float GetForward() {
-            return Vector3.Dot(body.velocity, transform.forward);
-        }
-
-        private float GetSide() {
-            return Vector3.Dot(body.velocity, transform.right);
         }
 
         public void setAcceptInput(bool option) {
@@ -90,10 +54,11 @@ namespace Game.Core.Driving {
 
                 float myMomentum    = Mathf.Abs(Vector3.Dot(body.velocity,      colDir)) * body.mass;
                 float otherMomentum = Mathf.Abs(Vector3.Dot(otherBody.velocity, colDir)) * otherBody.mass;
-                
+
                 if (myMomentum < otherMomentum) {
                     body.AddForce(colDir * otherMomentum * collisionAmplifier);
                 }
+
                 timestamp = Time.time + collisionCooldown;
             }
         }
